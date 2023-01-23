239c4198f925544c616ee623b4ed00387eb8adb7
Alex P. Scigalszky <Alex P. Scigalszky>
2023-01-23 15:21:18 -0300

    fix: try returning avalue on delete an item 4

--- netlify/functions/threethings-delete/index.ts
+++ netlify/functions/threethings-delete/index.ts
@@ -1,12 +1,8 @@
 import { Handler, HandlerEvent } from '@netlify/functions'
 
-// eslint-disable-next-line n/no-missing-import
-import { deleteById } from '../../../src/utils/spreadsheet_db'
-
 const handler: Handler = async (event: HandlerEvent) => {
   try {
     const data = JSON.parse(event.body)
-    // const result = await deleteById(data.identifier)
 
     return {
       statusCode: 200,
