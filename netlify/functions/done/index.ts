6741a55dc57853dd0344a2f762349391d91063a9
Alex P. Scigalszky <Alex P. Scigalszky>
2023-03-01 17:30:03 -0300

    feat: done list, delete

---
+++ netlify/functions/done/index.ts
@@ -0,0 +1,26 @@
+import { Handler } from '@netlify/functions'
+
+// eslint-disable-next-line n/no-missing-import
+import { getRows } from '../../../src/database/done'
+
+const handler: Handler = async () => {
+  try {
+    const list = await getRows()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(list),
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error.toString(),
+    }
+  }
+}
+
+export { handler }
