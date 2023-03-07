a8be60586647c185e543bf72eafa069a178e56d0
Alex P. Scigalszky <Alex P. Scigalszky>
2023-03-07 12:06:37 -0300

    feat: add date

--- src/database/ttt.ts
+++ src/database/ttt.ts
@@ -30,6 +30,7 @@ export const getRows = async (): Promise<ThreeThings[]> => {
 export const addRow = async (data: ThreeThings) => {
   const dataWithId = {
     identifier: generateUniqueId(),
+    date: new Date().toUTCString(),
     favorites: data.favorites ? data.favorites : 0,
     ...data,
   }
